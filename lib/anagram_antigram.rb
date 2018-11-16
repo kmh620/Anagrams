4172dd49bb93dcbac80c363fb82bf070dc56a1c4
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 09:32:32 -0800

    Pass second test, causes fail in first bc return statement

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -2,8 +2,7 @@ class AnagramAntigram
   def initialize(input)
     @input = input
   end
-  
-end
+  end
 
 
 
@@ -11,5 +10,10 @@ end
   def anagram_antigram(word)
     check_array = word.upcase().split("")
 
-check_array
+    check_array.each do |vowel|
+      if vowel === "A" || vowel === "E" || vowel === "I" || vowel === "O" || vowel === "U" || vowel === "Y"
+        return "This is a word"
+       end
+      "This is not a word"
+    end
 end
