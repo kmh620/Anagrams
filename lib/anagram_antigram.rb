cd09b915a8f25bdddf903f36c851761eed4cc73c
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 09:42:34 -0800

    Add third test- need to adjust 2nd

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -9,11 +9,15 @@ class AnagramAntigram
 
   def anagram_antigram(word)
     check_array = word.upcase().split("")
+      check_array
+  end
 
-    check_array.each do |vowel|
-      if vowel === "A" || vowel === "E" || vowel === "I" || vowel === "O" || vowel === "U" || vowel === "Y"
-        return "This is a word"
-       end
-      "This is not a word"
-    end
-end
+
+
+  # def is_word(check_array)
+  #   check_array.each do |vowel|
+  #     if vowel === "A" || vowel === "E" || vowel === "I" || vowel === "O" || vowel === "U" || vowel === "Y"
+  #       return "This is a word"
+  #      end
+  #     "This is not a word"
+  #   end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -8,8 +8,13 @@ describe ('anagram_antigram') do
   it('Changes all letters of input to uppercase, creates in array') do
     expect(anagram_antigram("Hello")).to(eq(["H", "E", "L", "L", "O"]))
   end
-
-  it ('Checks if input is a word- looks for vowels') do
-    expect(anagram_antigram("Hello")).to(eq("This is a word"))
+  it('Checks if two words are anagrams') do
+    expect(anagram_antigram("tea", "eat")).to(eq(true))
   end
 end
+#
+# describe ('is_word') do
+#   it ('Checks if input is a word- looks for vowels') do
+#     expect(anagram_antigram("Hello")).to(eq("This is a word"))
+#   end
+# end
