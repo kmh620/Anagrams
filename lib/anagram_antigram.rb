e747528ee425a041fe5cf7e2ab0546f8c634fab3
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 08:40:56 -0800

    Add first test

---
+++ lib/anagram_antigram.rb
@@ -0,0 +1,15 @@
+class AnagramAntigram
+  def initialize(input)
+    @input = input
+  end
+  
+end
+
+
+
+
+  def anagram_antigram(word)
+    check_array = word.upcase().split("")
+
+check_array
+end
---
+++ spec/anagram_antigram_spec.rb
@@ -0,0 +1,11 @@
+
+require ('rspec')
+require ('pry')
+require ('anagram_antigram.rb')
+
+describe ('anagram_antigram') do
+
+  it('Changes all letters of input to uppercase, creates in array') do
+    expect(anagram_antigram("Hello")).to(eq("H, E, L, L, O"))
+  end
+end
