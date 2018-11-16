c15cfd20512dc0e87b315d671a5ecbc53e01e860
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 12:31:10 -0800

    Pass 1/5 tests

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -7,17 +7,33 @@ class AnagramAntigram
 
 
 
-  def anagram(words)
-    temp_array = []
-    words_array = words.downcase.split("")
-    words_array.each do |letter|
-    if letter.include?('a' || 'e' || 'i' || 'o' || 'u' || 'y')
-      return true
+  def anagram(word_one, word_two)
+      if
+        word_one.downcase.split("").sort.join == word_two.downcase.split("").sort.join
+         "Anagram"
+      end
+     end
+    # output_array = Array.new(0)
+    # words.each do |w2|
+    #   if(w2.downcase.split(//).sort == w1.downcase.split(//).sort)
+    #     temp_array.push(w2)
+    #   end
+    #  end
+    #  output_array.push(temp_array)
+    #  return output_array.uniq
+    # end
 
+
+    # temp_array = []
+    # words_array = words.downcase.split("").sort
+    # # words_array.each do |letter|
+    # # if letter ==  ('a' || 'e' || 'i' || 'o' || 'u' || 'y')
+    # #   return true
+    # return words_array
     # temp_array.push(words_array.delete_if{|i| i == "." || i == "," || i == "?" || i == "!" || i == ":" || i == "'"}.join(" "))
    # word_one.chars.sort.join == word_two.chars.sort.join
 
-end
+
 
     # Use this later??
   #   check_array = []
@@ -28,5 +44,3 @@ end
   #   check_array
   # end
   #  End Use this later??
-end
-end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -6,18 +6,18 @@ require ('anagram_antigram.rb')
 describe ('anagram') do
 
   it('Changes input to same capitalization, create array') do
-    expect(anagram("Tea")).to(eq(["t", "e", "a"]))
+    expect(anagram("Tea", "EAt")).to(eq(["t", "e", "a"]))
   end
   it('Check if input is word') do
-    expect(anagram("tea")).to(eq(true))
+    expect(anagram("tea", "EAt")).to(eq(true))
   end
   it('Check if inputs are Anagrams') do
-    expect(anagram("tea eat")).to(eq("Anagram"))
+    expect(anagram("tea", "eat")).to(eq("Anagram"))
   end
   it('Check if inputs are Antigrams') do
-    expect(anagram("hi bye")).to(eq("Antigram"))
+    expect(anagram("hi", "bye")).to(eq("Antigram"))
   end
   it('Check for/ does not include punctuation in anagram/antigram') do
-    expect(anagram("hi, !, tea?, eat, bye")).to(eq("'hi', 'tea', 'eat', 'bye'"))
+    expect(anagram("hi", "!", "tea?", "eat", "bye")).to(eq("'hi', 'tea', 'eat', 'bye'"))
   end
 end
