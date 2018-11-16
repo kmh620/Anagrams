8991979905c47299f21983ebcc1bd8525dbb7168
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 14:25:46 -0800

    Adjust tests; for new direction

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -4,6 +4,7 @@ require ('pry')
 require ('anagram_antigram.rb')
 
 describe ('anagram') do
+
   it('Check if inputs are Anagrams') do
     expect(anagram("tea", "eat")).to(eq("Anagram"))
   end
@@ -11,16 +12,22 @@ describe ('anagram') do
   it('Changes input to same capitalization, create array') do
     expect(anagram("Tea", "EAt")).to(eq("Anagram"))
   end
+end
+
+describe ('is_word') do
 
   it('Check if input is word') do
-    expect(anagram("thbr", "wuww")).to(eq("Not a word"))
-  end
-
-  it('Check if inputs are Antigrams') do
-    expect(anagram("hi", "bye")).to(eq("Antigram"))
-  end
-
-  it('Check for/ does not include punctuation in anagram/antigram') do
-    expect(anagram("hi", "!")).to(eq("hi!"))
+    expect(is_word("eat")).to(eq("Is word"))
   end
 end
+
+describe ('antigram')
+  it('Check if inputs are Antigrams') do
+    expect(antigram("hi", "bye")).to(eq("Antigram"))
+  end
+end
+
+#   it('Check for/ does not include punctuation in anagram/antigram') do
+#     expect(anagram("hi", "!")).to(eq("hi"))
+#   end
+# end
