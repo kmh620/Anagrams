cfdc2be7a314ccdb4d70e403c59800b1c6fcf268
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 10:40:08 -0800

    Attempt word compare

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -7,12 +7,17 @@ class AnagramAntigram
 
 
 
-  def anagram_antigram(word)
-    check_array = []
-    word do |word1|
-      if (word1.upcase.split(//).sort == word2.upcase.split(//).sort)
-      check_array.push("Anagram")
-    end
-    check_array
-  end
+  def anagram(word_one, word_two)
+
+   word_one.chars.sort.join == word_two.chars.sort.join
 end
+
+    # Use this later??
+  #   check_array = []
+  #   word do |word1|
+  #     if (word1.upcase.split(//).sort == word2.upcase.split(//).sort)
+  #     check_array.push("Anagram")
+  #   end
+  #   check_array
+  # end
+  #  End Use this later??
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -6,18 +6,18 @@ require ('anagram_antigram.rb')
 describe ('anagram') do
 
   it('Changes input to same capitalization') do
-    expect(anagram_antigram("Hello")).to(eq("HELLO"))
+    expect(anagram("Hello")).to(eq("HELLO"))
   end
   it('Check if input is word') do
-    expect(anagram_antigram("Hello")).to(eq(true))
+    expect(anagram("Hello")).to(eq(true))
   end
   it('Check if inputs are Anagrams') do
-    expect(anagram_antigram("tea, eat")).to(eq(true))
+    expect(anagram("tea", "eat")).to(eq(true))
   end
   it('Check if inputs are Antigrams') do
-    expect(anagram_antigram("hi, bye")).to(eq(true))
+    expect(anagram("hi, bye")).to(eq(true))
   end
   it('Check multiple word string for anagrams & antigrams') do
-    expect(anagram_antigram("hi, tea, eat, bye")).to(eq(true))
+    expect(anagram("hi, tea, eat, bye")).to(eq(true))
   end
 end
