2243e66b3ac841b379ef9c73d740cd48923966e3
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 08:42:29 -0800

    Pass first test

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -6,6 +6,6 @@ require ('anagram_antigram.rb')
 describe ('anagram_antigram') do
 
   it('Changes all letters of input to uppercase, creates in array') do
-    expect(anagram_antigram("Hello")).to(eq("H, E, L, L, O"))
+    expect(anagram_antigram("Hello")).to(eq(["H", "E", "L", "L", "O"]))
   end
 end
