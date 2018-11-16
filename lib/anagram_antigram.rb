d57d5a74be1ffa1c8fd5aca4ef7f99001ece6e90
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 14:21:25 -0800

    Purposefull fail, see 2 tests pass

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -7,24 +7,26 @@ class AnagramAntigram
 
 
   def anagram(word_one, word_two)
-
-    temp_array = []
-    word1_array = word_one.downcase.split("").sort
-    word2_array = word_two.downcase.split("").sort
-
-    temp_array.push(word1_array.delete_if{|i| i == "." || i == "," || i == "?" || i == "!" || i == ":" || i == "'"}.join(" ")).push(word2_array.delete_if{|i| i == "." || i == "," || i == "?" || i == "!" || i == ":" || i == "'"}.join(" "))
-
-     if word_one.to_s !~ /a|e|i|o|u|y/ || word_two.to_s !~ /a|e|i|o|u|y/
-       "Not a word"
-     elsif
-       word_one.downcase.split("").sort.join == word_two.downcase.split("").sort.join
-        "Anagram"
-
-
-
-
-     else
-       "Antigram"
-     end
-
+    word_one.downcase.split("").sort.join == word_two.downcase.split("").sort.join
+     "Anagram!"
    end
+   #  temp_array = []
+   #  word1_array = word_one.downcase.split("").sort
+   #  word2_array = word_two.downcase.split("").sort
+   #
+   #  def new
+   #    temp_array.push(word1_array.delete_if{|i| i == "." || i == "," || i == "?" || i == "!" || i == ":" || i == "'"}.join(" ")).push(word2_array.delete_if{|i| i == "." || i == "," || i == "?" || i == "!" || i == ":" || i == "'"}.join(" "))
+   #
+   #  def if word_one.to_s !~ /a|e|i|o|u|y/ || word_two.to_s !~ /a|e|i|o|u|y/
+   #     "Not a word"
+   #  def elsif
+   #     word_one.downcase.split("").sort.join == word_two.downcase.split("").sort.join
+   #      "Anagram"
+   #
+   #
+   #
+   #
+   #  def  "Antigram"
+   #   end
+   #
+   # end
