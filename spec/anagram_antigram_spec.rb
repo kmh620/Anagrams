ba35cf001a90969f90a09dce8f4db26fa904019a
Katrina Hockman <khockman90@hotmail.com>
2018-11-16 08:44:25 -0800

    Add second test

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -8,4 +8,8 @@ describe ('anagram_antigram') do
   it('Changes all letters of input to uppercase, creates in array') do
     expect(anagram_antigram("Hello")).to(eq(["H", "E", "L", "L", "O"]))
   end
+
+  it ('Checks if input is a word- looks for vowels') do
+    expect(anagram_antigram("Hello")).to(eq("This is a word"))
+  end
 end
