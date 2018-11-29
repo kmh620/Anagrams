6fd3d5b11de6531c8ad55279a17283ed195ee585
Katrina Hockman <khockman90@hotmail.com>
2018-11-28 18:49:42 -0800

    Add Class

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -3,22 +3,23 @@ class AnagramAntigram
   @word_one = word_one
   @word_two = word_two
   end
-end
 
-def anagram(word_one, word_two)
-  word_one.gsub(/[[:punct:]]/, '').downcase.split("").sort.join == word_two.gsub(/[[:punct:]]/, '').downcase.split("").sort.join
-   "Anagram"
- end
 
-def is_word(word_one, word_two)
-  if word_one.to_s =~ /a|e|i|o|u|y/ || word_two.to_s =~ /a|e|i|o|u|y/
-      "Is word"
-  elsif word_one.to_s !~ /a|e|i|o|u|y/ || word_two.to_s !~ /a|e|i|o|u|y/
-      "Is not word"
+  def anagram
+    word_one.gsub(/[[:punct:]]/, '').downcase.split("").sort.join == word_two.gsub(/[[:punct:]]/, '').downcase.split("").sort.join
+     "Anagram"
+   end
+
+  def is_word
+    if word_one.to_s =~ /a|e|i|o|u|y/ || word_two.to_s =~ /a|e|i|o|u|y/
+        "Is word"
+    elsif word_one.to_s !~ /a|e|i|o|u|y/ || word_two.to_s !~ /a|e|i|o|u|y/
+        "Is not word"
+    end
   end
-end
 
-def antigram(word_one, word_two)
-  word_one.downcase.split("").sort.join != word_two.downcase.split("").sort.join
-    "Antigram"
+  def antigram
+    word_one.downcase.split("").sort.join != word_two.downcase.split("").sort.join
+      "Antigram"
+  end
 end
