053fba373863201049f82975f4dd9c9bfb199442
Katrina Hockman <khockman90@hotmail.com>
2018-12-02 14:48:44 -0800

    Add new test, pass

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,40 +1,44 @@
-class AnagramAntigram
+class Anagram
 
-  attr_accessor(:word1, :word2)
+  # attr_accessor(:word1, :word2)
 
   def initialize(word1, word2)
     @word1 = word1
     @word2 = word2
   end
 
+
   def remove_punct(word)
+      no_punct = word.gsub(/[[:punct:]]/, '').downcase.split("").sort.join
+      if no_punct != nil
+        return no_punct
+      end
+  end
 
-    if word.gsub(/[[:punct:]]/, '').downcase.split("").sort.join
-   end
- end
-
-  def is_word(word1, word2)
-    if word1.to_s !~ /a|e|i|o|u|y/ || word2.to_s !~ /a|e|i|o|u|y/
+  def is_word(word)
+    this_word = word
+    if this_word.to_s !~ /a|e|i|o|u|y/
         return false
     else
       return true
     end
   end
 
-  def check_anagram
-    no_punct1 = remove_punct(@word1)
-    no_punct2 = remove_punct(@word2)
-    check_word1 = is_word(no_punct1)
-    check_word2 = is_word(no_punct2)
+end
 
-    if check_word1 == true && check_word2 == true
-      if (check_word1 && check_word2).empty?
-      return  "Antigrams"
-    elsif !(check_word1 && check_word2).empty?
-       check_word1.length == check_word2.length
-       return "Anagrams"
-     else "Neither"
-     end
-    end
-  end
- end
+#
+#   def check_anagram
+#     check_word1 = is_word.remove_pun(@word1)
+#     check_word2 = is_word(@word2)
+#
+#     if check_word1 == true && check_word2 == true
+#       if (check_word1 && check_word2).empty?
+#       return  "Antigrams"
+#     elsif !(check_word1 && check_word2).empty?
+#        check_word1.length == check_word2.length
+#        return "Anagrams"
+#      else "Neither"
+#      end
+#     end
+#   end
+# end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -3,37 +3,14 @@ require ('rspec')
 require ('pry')
 require ('anagram_antigram.rb')
 
-describe ('#anagram_antigram') do
-  it('Changes input to same capitalization, create array') do
-    test1 = AnagramAntigram.new("word1", "word2")
-    expect(test1.remove_punct("TEA", "eAt")).to(eq("tea", "eat"))
-  end
-  it('Removes punctuation') do
-    word2 = AnagramAntigram.new("word1", "word2")
-    expect(word2.remove_punct("!g@od..", "dog!!:,")).to(eq("god", "dog"))
-  end
-end
 
-describe ('is_word') do
-  it('Check if input is word') do
-    word = AnagramAntigram.new("word1", "word2")
-    expect(word.is_word("hello", "goodbye")).to(eq(true))
-    words = AnagramAntigram.new("word1", "word2")
-    expect(words.is_word("ththth", "rrr")).to(eq(false))
-  end
-end
 
-describe ('check_anagram') do
-  it('Check if inputs are Antigrams') do
-    word1 = AnagramAntigram.new("word1", "word2")
-    expect(word1.check_anagram("Hi", "Bye")).to(eq("Antigram"))
+describe('#anagram_antigram') do
+  it('Remove punctuation from input words') do
+    test1 = Anagram.new("word1", "word2")
+    expect(test1.remove_punct("d#og!,,")).to(eq("dgo"))
   end
-  it('Check if inputs are Anagrams') do
-    word2 = AnagramAntigram.new("word1", "word2")
-    expect(word2.check_anagram("cat", "act")).to(eq("Anagram"))
-  end
-  it('Check if inputs are Neither') do
-    word3 = AnagramAntigram.new("word1", "word2")
-    expect(word3.check_anagram("Hello", "Help")).to(eq("neither"))
-  end
+  
+
+
 end
