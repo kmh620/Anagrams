5257b933195edd78212db8c9073050dcfca188dc
Katrina Hockman <khockman90@hotmail.com>
2018-12-02 14:54:25 -0800

    Add 2nd test, pass- checks voewls in word

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -6,11 +6,17 @@ require ('anagram_antigram.rb')
 
 
 describe('#anagram_antigram') do
-  it('Remove punctuation from input words') do
+  it('Remove punctuation from input words, sorts') do
     test1 = Anagram.new("word1", "word2")
     expect(test1.remove_punct("d#og!,,")).to(eq("dgo"))
   end
-  
+
+  it('Check if word contains vowels = is a word') do
+    test2 = Anagram.new("word1", "word2")
+    expect(test2.is_word("cat")).to(eq(true))
+    test3 = Anagram.new("word1", "word2")
+    expect(test2.is_word("tthth")).to(eq(false))
+  end
 
 
 end
